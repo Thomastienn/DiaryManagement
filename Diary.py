b6365708c4c43eb02d5c192e36a2394f547f3719
Thomas <thomastien123@gmail.com>
2023-03-06 14:02:50 -0500

    Finish break down long words into a list

--- Diary.py
+++ Diary.py
@@ -3,15 +3,18 @@ import ast
 import rsa
 from datetime import datetime, timedelta
 
+# Constants
 CURRENT_YEAR = datetime.now().strftime("20%y")
 PRIVATE_KEYS_DIR = "D:\\keys\\private_key.pem"
 MILESTONE_DIR = f"D:\\Diary\\{CURRENT_YEAR}\\Milestone"
 EXTRA = ["SONGS", "MOVIES", "DAILY LIFE", "NEW FOOD", "NEW PEOPLE", "NEW PLACES", "NEW ACTIVITY", "TRENDING", "VERSIONS"]
 
 def decrypt_rsa_message(ciphertext_file, private_key_file):
+    # Get private key
     with open(private_key_file, 'rb') as key_file:
         private_key = rsa.PrivateKey.load_pkcs1(key_file.read())
 
+    # If there is the file
     try:
         with open(ciphertext_file, 'rb') as f:
             returnText = ""
@@ -20,9 +23,14 @@ def decrypt_rsa_message(ciphertext_file, private_key_file):
 
             list = ciphertext.decode("utf-8").split("\n")
 
+            # Decrypt by each line
             for line in list:
+                # If the line is valid
                 if(len(line) != 0):
                     try:
+                        # line is string representation of byte
+                        # Ex: b'ads\123\saf\bsbs'
+                        # ast -> turns into real byte
                         plaintext = rsa.decrypt(ast.literal_eval(line), private_key)
                         returnText += plaintext.decode("utf-8") + "\n"
                     except:
@@ -31,16 +39,41 @@ def decrypt_rsa_message(ciphertext_file, private_key_file):
     except Exception:
         return "You didn't write diary this date"
 
+def break_long_string_into_list(text):
+    words_list = text.split(" ")
+    final_res = []
+    curNumBytes = 0
+    MAX_BYTES = 240
+    line_text = ""
+
+    for word in words_list:
+        real_word = word + " "
+        curNumBytes += utf8len(real_word)
+        if(curNumBytes > MAX_BYTES):
+            final_res.append(line_text)
+            curNumBytes = 0
+            line_text = ""
+        line_text += real_word
+    final_res.append(line_text)
+
+    return final_res
+
 def write_to_file_with_encryption(filename, text):
+    # Get public key
     with open("D:\\keys\\public_key.pem", "rb") as key_file:
         public_key = rsa.PublicKey.load_pkcs1(key_file.read())
 
-    message = text.encode("utf-8")
-    encrypted_text = rsa.encrypt(message, public_key)
+    for piece in break_long_string_into_list(text):
+        # piece: string -> into message: byte
+        message = piece.encode("utf-8")
+        encrypted_text = rsa.encrypt(message, public_key)
 
-    mode = "a" if os.path.exists(filename) else "w"
-    with open(filename, mode, encoding="utf-8") as file:
-        file.write(str(encrypted_text) + "\n")
+        # Create a new file if it's not existed
+        mode = "a" if os.path.exists(filename) else "w"
+
+        # Write the string representation of the byte
+        with open(filename, mode, encoding="utf-8") as file:
+            file.write(str(encrypted_text) + "\n")
     print("Success")
 
 def printMenu():
@@ -50,6 +83,8 @@ def printMenu():
     print("0. Exit")
     print("1. Write")
     print("2. Read")
+    print("3. Keys")
+    print("4. Find")
     print("-"*WIDTH)
 
 def printMilestoneMenu():
@@ -66,6 +101,8 @@ def printTitle(mes):
     print(mes)
     print("-"*10)
 
+def utf8len(s):
+    return len(s.encode('utf-8'))
 
 def run():
     while True:

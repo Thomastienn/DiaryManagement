0d7a8b404248a4a0d65b48ab5627b0c3196414d7
Thomas <thomastien123@gmail.com>
2023-03-07 08:21:21 -0500

    Reconstruct the main func

--- Diary.py
+++ Diary.py
@@ -4,9 +4,14 @@ import rsa
 from datetime import datetime, timedelta
 
 # Constants
+DIARY_DIR = "D:\\Diary"
+KEYS_DIR = "D:\\keys"
+
+PRIVATE_KEYS_DIR = f"{KEYS_DIR}\\private_key.pem"
+
 CURRENT_YEAR = datetime.now().strftime("20%y")
-PRIVATE_KEYS_DIR = "D:\\keys\\private_key.pem"
-MILESTONE_DIR = f"D:\\Diary\\{CURRENT_YEAR}\\Milestone"
+MILESTONE_DIR = f"{DIARY_DIR}\\{CURRENT_YEAR}\\Milestone"
+
 EXTRA = ["SONGS", "MOVIES", "DAILY LIFE", "NEW FOOD", "NEW PEOPLE", "NEW PLACES", "NEW ACTIVITY", "TRENDING", "VERSIONS"]
 
 def decrypt_rsa_message(ciphertext_file, private_key_file):
@@ -36,24 +41,33 @@ def decrypt_rsa_message(ciphertext_file, private_key_file):
                     except:
                         continue
             return returnText
-    except Exception:
-        return "You didn't write diary this date"
+    except (FileNotFoundError, FileExistsError):
+        return "You didn't write diary this date\n"
 
 def break_long_string_into_list(text):
+    # Split text into list of words
     words_list = text.split(" ")
+
+    MAX_BYTES = 240
+    
     final_res = []
     curNumBytes = 0
-    MAX_BYTES = 240
     line_text = ""
 
     for word in words_list:
         real_word = word + " "
         curNumBytes += utf8len(real_word)
+
+        # If it exceeds MAX_BYTES then we have to break
         if(curNumBytes > MAX_BYTES):
+            # Start a new string
             final_res.append(line_text)
             curNumBytes = 0
             line_text = ""
         line_text += real_word
+
+    # There is still left in the line_text that 
+    # hasn't been appended
     final_res.append(line_text)
 
     return final_res
@@ -104,88 +118,97 @@ def printTitle(mes):
 def utf8len(s):
     return len(s.encode('utf-8'))
 
+def write_main():
+    dir = ""
+    timeStamp = ""
+
+    user_choose = input("Diary or milestone (D/M): ")
+
+    if(user_choose == "d"):
+        today=datetime.now().strftime("%d-%m")
+        dir = "D:\\Diary\\" + datetime.now().strftime("20%y") + "\\" + today + ".txt"
+        timeStamp = datetime.now().strftime("[%H:%M:%S]") + ": "
+    else:
+        os.system('cls')
+        printMilestoneMenu()
+
+        try:
+            user_milestone_select = int(input("Choose: "))
+        except:
+            user_milestone_select = 0
+
+        if(user_milestone_select != 0):
+            nm = "_".join(EXTRA[user_milestone_select-1].split(" "))
+            dir = f"{MILESTONE_DIR}\\{nm}.txt"
+            timeStamp = datetime.now().strftime("[%d/%m/20%y %H:%M:%S]") + ": "
+
+    userText = input("Enter txt to write to file: ")
+
+    if(len(userText) == 0):
+        print("Empty message")
+    else:
+        write_to_file_with_encryption(dir, timeStamp + userText)
+
+def read_main():
+    user_choose = input("Diary or milestone (D/M): ")
+    dir = ""
+
+    if(user_choose == "d"):
+        today=datetime.now()
+
+        while True:
+            todayString = today.strftime("%d-%m")
+            todayYear = today.strftime("20%y")
+            os.system("cls")
+
+            printTitle(f"{todayString}-{todayYear}")
+            dir = f"D:\\Diary\\{todayYear}\\{todayString}.txt"
+
+            decrypted_message = decrypt_rsa_message(dir, PRIVATE_KEYS_DIR)
+            print(decrypted_message)
+
+            user_choose = input("NAV: ")
+            if(user_choose == "a"):
+                today = (today - timedelta(days=1))
+            elif(user_choose == "d"):
+                today = (today + timedelta(days=1))
+            else:
+                try:
+                    today = datetime.strptime(user_choose[:-5] + "-" + user_choose[-2:], "%d-%m-%y")
+                except Exception:
+                    break
+        
+    else:
+        os.system('cls')
+        printMilestoneMenu()
+
+        try:
+            user_milestone_select = int(input("Choose: "))
+        except:
+            user_milestone_select = 0
+
+        if(user_milestone_select != 0):
+            nm = "_".join(EXTRA[user_milestone_select-1].split(" "))
+            dir = f"{MILESTONE_DIR}\\{nm}.txt"
+
+            decrypted_message = decrypt_rsa_message(dir, PRIVATE_KEYS_DIR)
+            print(decrypted_message)
+
+def default():
+    print("In development\n")
+
+options = {
+    "1": write_main,
+    "2": read_main,
+}
+
 def run():
     while True:
         os.system('cls')
         printMenu()
         user_input = input("Choose an option: ")
-        if(user_input == "0"):
-            return
-        elif(user_input == "1"):
-            dir = ""
-            timeStamp = ""
 
-            user_choose = input("Diary or milestone (D/M): ")
-
-            if(user_choose == "d"):
-                today=datetime.now().strftime("%d-%m")
-                dir = "D:\\Diary\\" + datetime.now().strftime("20%y") + "\\" + today + ".txt"
-                timeStamp = datetime.now().strftime("[%H:%M:%S]") + ": "
-            else:
-                os.system('cls')
-                printMilestoneMenu()
-
-                try:
-                    user_milestone_select = int(input("Choose: "))
-                except:
-                    user_milestone_select = 0
-
-                if(user_milestone_select != 0):
-                    nm = "_".join(EXTRA[user_milestone_select-1].split(" "))
-                    dir = f"{MILESTONE_DIR}\\{nm}.txt"
-                    timeStamp = datetime.now().strftime("[%d/%m/20%y %H:%M:%S]") + ": "
-
-            userText = input("Enter txt to write to file: ")
-
-            if(len(userText) == 0):
-                print("Empty message")
-            else:
-                write_to_file_with_encryption(dir, timeStamp + userText)
-
-        elif(user_input == "2"):
-            user_choose = input("Diary or milestone (D/M): ")
-            dir = ""
-
-            if(user_choose == "d"):
-                today=datetime.now()
-
-                while True:
-                    todayString = today.strftime("%d-%m")
-                    todayYear = today.strftime("20%y")
-                    os.system("cls")
-
-                    printTitle(f"{todayString}-{todayYear}")
-                    dir = f"D:\\Diary\\{todayYear}\\{todayString}.txt"
-
-                    decrypted_message = decrypt_rsa_message(dir, PRIVATE_KEYS_DIR)
-                    print(decrypted_message)
-
-                    user_choose = input("NAV: ")
-                    if(user_choose == "a"):
-                        today = (today - timedelta(days=1))
-                    elif(user_choose == "d"):
-                        today = (today + timedelta(days=1))
-                    else:
-                        try:
-                            today = datetime.strptime(user_choose[:-5] + "-" + user_choose[-2:], "%d-%m-%y")
-                        except Exception:
-                            break
-                
-            else:
-                os.system('cls')
-                printMilestoneMenu()
-
-                try:
-                    user_milestone_select = int(input("Choose: "))
-                except:
-                    user_milestone_select = 0
-
-                if(user_milestone_select != 0):
-                    nm = "_".join(EXTRA[user_milestone_select-1].split(" "))
-                    dir = f"{MILESTONE_DIR}\\{nm}.txt"
-
-                    decrypted_message = decrypt_rsa_message(dir, PRIVATE_KEYS_DIR)
-                    print(decrypted_message)
+        options.get(user_input, default)()
 
         print("Press to continue...")
         input()
